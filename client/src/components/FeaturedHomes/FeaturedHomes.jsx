7427b6673902feefe3591eb19a708c77f7d5790d
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-03-23 17:22:58 +0530

    Add featured homes

---
+++ client/src/components/FeaturedHomes/FeaturedHomes.jsx
@@ -0,0 +1,42 @@
+import './FeaturedHomes.css'
+
+function FeaturedHomes() {
+  return (
+    <div className='featuredHomes'>
+      <div className="featuredHomeItem">
+        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" alt="" className="featuredHomeImage" />
+        <span className="featuredHomeName">Pearl Villa</span>
+        <span className="featuredHomeCity">Matara</span>
+        <span className="featuredHomePrice">Starting from $60</span>
+        <div className="featuredHomeRating">
+          <button>7.5</button>
+          <span>Good</span>
+        </div>
+      </div>
+
+      <div className="featuredHomeItem">
+        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" alt="" className="featuredHomeImage" />
+        <span className="featuredHomeName">Pearl Villa</span>
+        <span className="featuredHomeCity">Matara</span>
+        <span className="featuredHomePrice">Starting from $60</span>
+        <div className="featuredHomeRating">
+          <button>7.5</button>
+          <span>Good</span>
+        </div>
+      </div>
+
+      <div className="featuredHomeItem">
+        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" alt="" className="featuredHomeImage" />
+        <span className="featuredHomeName">Pearl Villa</span>
+        <span className="featuredHomeCity">Matara</span>
+        <span className="featuredHomePrice">Starting from $60</span>
+        <div className="featuredHomeRating">
+          <button>7.5</button>
+          <span>Good</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FeaturedHomes
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Featured from '../../components/Featured/Featured';
+import FeaturedHomes from '../../components/FeaturedHomes/FeaturedHomes';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
 import PropertyList from '../../components/propertyList/PropertyList';
@@ -15,6 +16,10 @@ const Home = () => {
           Browse by property type
         </h1>
         <PropertyList />
+        <h1 className="homeTitle">
+        Homes guests love
+        </h1>
+        <FeaturedHomes />
       </div>
     </div>
   )
